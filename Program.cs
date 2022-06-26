56c84de150d684c3f74c2decaf2765fbb346b689
Yung.Energy <morskojroma@gmail.com>
2022-06-26 20:05:48 +0300

    Task3

---
+++ Program.cs
@@ -0,0 +1,11 @@
+﻿Console.Write("Введите число: ");
+int a = Convert.ToInt32(Console.ReadLine());
+int b = a%2;
+if (b > 0)
+{
+    Console.WriteLine("Число не четное");
+}
+else
+{
+    Console.WriteLine("Число четное");
+}
